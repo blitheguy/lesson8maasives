6b02e1e288e9b80000fb26194dd308e44d16e3b7
blitheguy <103589017+blitheguy@users.noreply.github.com>
2022-04-17 17:58:37 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int max = arr[0];
+        for (int i = 1; i < 6; i++) {
+            if (arr[i] > max) max = arr[i];
+        }
+        System.out.println("Самое большое число=" + max);
+    }
+}
